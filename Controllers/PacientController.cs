d44c04863fc749a2e0092e1eb28de754249dbc3b
TovCrowe <manuelxdvic@gmail.com>
2024-05-05 21:22:00 -0600

    Finished all controllers

--- Controllers/PacientController.cs
+++ Controllers/PacientController.cs
@@ -67,5 +67,40 @@ namespace api.Controllers
                 return StatusCode(500, ex);
             }   
         }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdatePatient([FromRoute] int id, [FromBody] UpdatePacientDTO updatePacientDTO){
+            try{
+                var patient = await _context.pacientes.FirstOrDefaultAsync(x => x.cliente_id == id);
+                if(patient == null){
+                    return NotFound();
+                }
+                patient.nombre = updatePacientDTO.nombre;
+                patient.email = updatePacientDTO.email;
+                patient.fecha_de_alta = updatePacientDTO.fecha_de_alta;
+                patient.sintomas = updatePacientDTO.sintomas;
+
+                await _context.SaveChangesAsync();
+                return Ok(patient.ToPatientDTO());
+
+            } catch (Exception ex){
+                return StatusCode(500, ex);
+            }
+        }  
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeletePatient([FromRoute] int id){
+            try{
+                var patient = await _context.pacientes.FirstOrDefaultAsync(x => x.cliente_id == id);
+                if(patient == null){
+                    return NotFound();
+                }
+                _context.pacientes.Remove(patient);
+                await _context.SaveChangesAsync();
+                return NoContent(); 
+            } catch (Exception ex){
+                return StatusCode(500, ex);
+            }
+        }
     }
 }
